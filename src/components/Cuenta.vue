183cdec19d38b841a4a26c949e4dbb40dc65be20
AnD <andersonventosilla0@gmail.com>
2021-08-15 17:37:15 -0500

    Primer commit

---
+++ src/components/Cuenta.vue
@@ -0,0 +1,76 @@
+<template>
+
+    <h2>Tipo de Cuenta: {{cuenta}}</h2>
+    <h2>Saldo: {{saldo}}</h2>
+    <h2>Cuenta: {{ estado ? 'Activa' : 'Desactivada'}}</h2>
+    <div v-for="(servicio,index) in servicios" :key="index">
+       {{ index + 1 }} -  {{ servicio }}
+    </div>
+    <AccionSaldo 
+        texto="Aumentar Saldo"
+        @accion="aumentar"
+    />
+    <AccionSaldo 
+        texto="Disminuir Saldo"
+        @accion="disminuir"
+        :desactivar="desactivar"
+    />
+    <AccionSaldo 
+        texto="Multiplicar por 2 el Saldo"
+        @accion="multiplicar"
+    />
+    <AccionSaldo
+        texto="Dividir el saldo en 2"
+        @accion="dividir"
+    />
+
+</template>
+
+<script >
+import AccionSaldo from './AccionSaldo.vue'
+
+
+export default {
+    components: {
+        AccionSaldo
+    },
+    data() {
+        return {
+            saldo: 1000,
+            cuenta: 'Visa',
+            estado: true,
+            cuenta: 'Visa',
+            servicios: ['giro','abono','transferencia'],
+            desactivar: false
+        }
+    },
+    methods: {
+
+        aumentar() {
+            this.saldo = this.saldo + 100
+            this.desactivar = false
+        },
+
+        disminuir() {
+            if(this.saldo === 0){
+                this.desactivar = true
+                alert('Saldo Agotado')
+                return 
+            }
+            this.saldo = this.saldo - 100
+        },
+
+        multiplicar() {
+            this.saldo = this.saldo * 2
+        },
+
+        dividir() {
+            this.saldo = this.saldo / 2
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Titulo.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>{{ texto }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'Titulo',
+    props:  {
+        texto: String
+    }
+}
+</script>
+
+// scoped para que solo cambie a ese h1 especificado y no globalmente
+<style scoped>                  
+
+    h1 {
+        color: peru;
+    }
+
+</style>
